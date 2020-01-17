e656575359053962b055034dc0dbe4c8d89fcdb7
Jiaji Li <jiajli@microsoft.com>
2020-01-17 11:23:03 -0800

    azure storage queue for fun

---
+++ QueueApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+
+namespace QueueApp {
+    class Program {
+        private const string connectionString = "your connection";
+
+        static async Task Main (string[] args) {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse (connectionString);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient ();
+            CloudQueue queue = queueClient.GetQueueReference ("pocrgstoragequeue");
+
+            if (args.Length > 0) {
+                string value = String.Join (" ", args);
+                await SendMessageAsync (queue, value);
+                Console.WriteLine ($"Sent: {value}");
+            } else {
+                string value = await ReceiveMessageAsync (queue);
+                Console.WriteLine ($"Received {value}");
+            }
+
+            Console.Write ("Press Enter...");
+            Console.ReadLine ();
+        }
+
+        static async Task SendMessageAsync (CloudQueue theQueue, string newMessage) {
+            bool createdQueue = await theQueue.CreateIfNotExistsAsync ();
+
+            if (createdQueue) {
+                Console.WriteLine ("The queue was created.");
+            }
+
+            CloudQueueMessage message = new CloudQueueMessage (newMessage);
+            await theQueue.AddMessageAsync (message);
+        }
+
+        static async Task<string> ReceiveMessageAsync (CloudQueue theQueue) {
+            bool exists = await theQueue.ExistsAsync ();
+
+            if (exists) {
+                CloudQueueMessage retrievedMessage = await theQueue.GetMessageAsync ();
+
+                if (retrievedMessage != null) {
+                    string theMessage = retrievedMessage.AsString;
+                    await theQueue.DeleteMessageAsync (retrievedMessage);
+                    return theMessage;
+                } else {
+                    Console.Write ("The queue is empty. Attempt to delete it? (Y/N) ");
+                    string response = Console.ReadLine ();
+
+                    if (response == "Y" || response == "y") {
+                        await theQueue.DeleteIfExistsAsync ();
+                        return "The queue was deleted.";
+                    } else {
+                        return "The queue was not deleted.";
+                    }
+                }
+            } else {
+                return "The queue does not exist. Add a message to the command line to create the queue and store the message.";
+            }
+        }
+    }
+}
